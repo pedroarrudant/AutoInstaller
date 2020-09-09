e6c9e7a8eca59dc6ad22a535619984778d4bf9c5
Pedro <pedro.arruda@outlook.com.br>
2020-09-09 11:55:36 -0300

    Corrigindo nomes e habilitando consultas assincronas

--- AutoInstaller/HomeForm.Designer.cs
+++ AutoInstaller/HomeForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AutoInstaller
 {
-    partial class Form1
+    partial class HomeForm
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -30,13 +30,15 @@
         {
             this.btnInstalar = new System.Windows.Forms.Button();
             this.dgvSoftwares = new System.Windows.Forms.DataGridView();
+            this.progressInstallation = new System.Windows.Forms.ProgressBar();
+            this.lblStatus = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvSoftwares)).BeginInit();
             this.SuspendLayout();
             // 
             // btnInstalar
             // 
             this.btnInstalar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnInstalar.Location = new System.Drawing.Point(310, 292);
+            this.btnInstalar.Location = new System.Drawing.Point(310, 347);
             this.btnInstalar.Name = "btnInstalar";
             this.btnInstalar.Size = new System.Drawing.Size(158, 50);
             this.btnInstalar.TabIndex = 2;
@@ -50,30 +52,51 @@
             this.dgvSoftwares.AllowUserToDeleteRows = false;
             this.dgvSoftwares.AllowUserToResizeColumns = false;
             this.dgvSoftwares.AllowUserToResizeRows = false;
-            this.dgvSoftwares.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvSoftwares.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvSoftwares.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvSoftwares.Location = new System.Drawing.Point(125, 7);
             this.dgvSoftwares.Name = "dgvSoftwares";
             this.dgvSoftwares.Size = new System.Drawing.Size(343, 279);
             this.dgvSoftwares.TabIndex = 4;
             // 
-            // Form1
+            // progressInstallation
+            // 
+            this.progressInstallation.Location = new System.Drawing.Point(125, 318);
+            this.progressInstallation.Name = "progressInstallation";
+            this.progressInstallation.Size = new System.Drawing.Size(343, 23);
+            this.progressInstallation.TabIndex = 5;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(125, 299);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(74, 13);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "Aguardando...";
+            // 
+            // HomeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(480, 354);
+            this.ClientSize = new System.Drawing.Size(480, 409);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.progressInstallation);
             this.Controls.Add(this.dgvSoftwares);
             this.Controls.Add(this.btnInstalar);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "HomeForm";
+            this.Text = "Auto Installer";
             ((System.ComponentModel.ISupportInitialize)(this.dgvSoftwares)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Button btnInstalar;
         private System.Windows.Forms.DataGridView dgvSoftwares;
+        private System.Windows.Forms.ProgressBar progressInstallation;
+        private System.Windows.Forms.Label lblStatus;
     }
 }
 
---
+++ AutoInstaller/HomeForm.cs
@@ -0,0 +1,132 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoInstaller
+{
+    public partial class HomeForm : Form
+    {
+        private readonly string downloadpath = @"C:\Users\Pedro\Downloads\";
+        private readonly List<Software> softwares;
+        public HomeForm()
+        {
+            InitializeComponent();
+
+            this.softwares = SerializeApps();
+            PopulateDataGridView();
+        }
+
+        private void PopulateDataGridView()
+        {
+            DataGridViewCell cell = new DataGridViewTextBoxCell();
+
+            DataGridViewCheckBoxColumn chk = new DataGridViewCheckBoxColumn
+            {
+                HeaderText = "Install",
+                Name = "chkinstall"
+            };
+
+            var columnName = new DataGridViewColumn
+            {
+                HeaderText = "Software Name",
+                Name = "columnName",
+                CellTemplate = cell
+            };
+
+            var columnSize = new DataGridViewColumn
+            {
+                HeaderText = "Software Size",
+                Name = "columnSize",
+                CellTemplate = cell
+            };
+
+            dgvSoftwares.Columns.Add(chk);
+            dgvSoftwares.Columns.Add(columnName);
+            dgvSoftwares.Columns.Add(columnSize);
+
+            foreach (var software in softwares)
+            {
+                object[] row = new object[] { false, software.nome, "0" + " MB" };
+                dgvSoftwares.Rows.Add(row);
+            }
+        }
+
+        private async void btnInstalar_Click(object sender, EventArgs e)
+        {
+            progressInstallation.Maximum = dgvSoftwares.RowCount;
+            foreach (DataGridViewRow row in dgvSoftwares.Rows)
+            {
+                foreach (var software in softwares)
+                {
+                    if (software.nome == row.Cells[1].Value.ToString() && row.Cells[0].Value.ToString() == "True")
+                    {
+                        lblStatus.Text = "Instalando " + software.nome + "...";
+                        Uri remoteUri = new Uri(software.caminhoweb);
+                        string fileName = software.nomearquivo;
+
+                        // Create a new WebClient instance.
+                        using (WebClient myWebClient = new WebClient())
+                        {
+                            // Download the Web resource and save it into the current filesystem folder.
+                            await myWebClient.DownloadFileTaskAsync(remoteUri, downloadpath + fileName);
+                        }
+
+                        //ProcessStartInfo info = new ProcessStartInfo();
+
+                        //info.FileName = downloadpath + fileName;
+                        //info.Arguments = software.formasileciosa;
+
+                        //using (Process exeProcess = Process.Start(info))
+                        //{
+                        //    exeProcess.WaitForExit();
+                        //}
+
+                        await RunProcessAsync(downloadpath + fileName, software.formasileciosa);
+                        lblStatus.Text = software.nome + " instalado com sucesso!";
+                    }
+                }
+                lblStatus.Text = "Todos os softwares selecionados foram instalados com sucesso!";
+                progressInstallation.Value += 1;
+            }
+        }
+
+        private List<Software> SerializeApps()
+        {
+            string filepath = "./AppList.json";
+            string text = File.ReadAllText(filepath);
+
+            var deserializedProduct = JsonConvert.DeserializeObject<List<Software>>(text);
+
+            return deserializedProduct;
+        }
+
+        public static async Task RunProcessAsync(string fileName, string arguments)
+        {
+            var tcs = new TaskCompletionSource<int>();
+
+            var process = new Process
+            {
+                StartInfo = { FileName = fileName, Arguments = arguments },
+                EnableRaisingEvents = true
+            };
+
+            process.Exited += (sender, args) =>
+            {
+                tcs.SetResult(process.ExitCode);
+                process.Dispose();
+            };
+
+            process.Start();
+        }
+    }
+}
